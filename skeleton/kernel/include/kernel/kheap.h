28dd56f13955bac5e2ca94ffa7ad97779aea74d6
Ahtif <ahtif.anwar@warwick.ac.uk>
2019-08-17 19:45:50 +0100

    Add placement malloc code for simple memory allocation

---
+++ skeleton/kernel/include/kernel/kheap.h
@@ -0,0 +1,42 @@
+// kheap.h -- Interface for kernel heap functions, also provides
+//            a placement malloc() for use before the heap is
+//            initialised.
+
+#ifndef _KERNEL_KHEAP_H
+#define _KERNEL_KHEAP_H
+
+/**
+   Allocate a chunk of memory, size in size. If align == 1,
+   the chunk must be page-aligned. If phys != 0, the physical
+   location of the allocated chunk will be stored into phys.
+
+   This is the internal version of kmalloc. More user-friendly
+   parameter representations are available in kmalloc, kmalloc_a,
+   kmalloc_ap, kmalloc_p.
+**/
+uint32_t kmalloc_int(uint32_t size, int align, uint32_t *phys);
+
+/**
+   Allocate a chunk of memory, size in size. The chunk must be
+   page aligned.
+**/
+uint32_t kmalloc_a(uint32_t size);
+
+/**
+   Allocate a chunk of memory, size in size. The physical address
+   is returned in phys. Phys MUST be a valid pointer to uint32_t!
+**/
+uint32_t kmalloc_p(uint32_t size, uint32_t *phys);
+
+/**
+   Allocate a chunk of memory, size in size. The physical address
+   is returned in phys. It must be page-aligned.
+**/
+uint32_t kmalloc_ap(uint32_t size, uint32_t *phys);
+
+/**
+   General allocation function.
+**/
+uint32_t kmalloc(uint32_t size);
+
+#endif // KHEAP_H
